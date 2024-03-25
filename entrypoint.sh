6e587feff182d60b466dce40490bcd4756b0e000
J. Renner <j@lerenner.dev>
2024-03-25 18:06:32 -0300

    Add ssh host keys and reorganize script
    Signed-off-by: J. Renner <j@lerenner.dev>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,21 @@
 mkdir -p /home/git/.ssh
 
 # Copy SSH public key to the appropriate location
-# Necessary because of unavailability of permissions for volume mounts in k8s
-cat /home/git/.ssh/volumeMounts/authorized_keys > /home/git/.ssh/authorized_keys
-
+cat /k8sMounts/user-public-key/authorized_keys > /home/git/.ssh/authorized_keys
 chown git:git /home/git/.ssh
 chown git:git /home/git/.ssh/authorized_keys
-
-# Set correct permissions
 chmod 700 /home/git/.ssh
 chmod 600 /home/git/.ssh/authorized_keys
 
+# Copy host SSH private keys to server location
+cat /k8sMounts/host-private-key/ssh_host_rsa_key > /etc/ssh/ssh_host_rsa_key
+cat /k8sMounts/host-private-key/ssh_host_ecdsa_key > /etc/ssh/ssh_host_ecdsa_key
+cat /k8sMounts/host-private-key/ssh_host_ed25519_key > /etc/ssh/ssh_host_ed25519_key
+chmod 600 /etc/ssh/ssh_host_rsa_key
+chmod 600 /etc/ssh/ssh_host_ecdsa_key
+chmod 600 /etc/ssh/ssh_host_ed25519_key
+
+# Set correct permissions to /projects directory
 chmod 755 /projects
 chown git:git /projects
 
